a220204fad0d3c029040dcc6b313f8c35545df5c
FrancoRosa <km115.franco@gmail.com>
2020-06-16 12:57:13 -0500

    Add create method for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    @comment = current_user.comments.new(post_id: params[:post_id], text: params[:comment][:text])
+    if @comment.save
+      redirect_to root_path, notice: "Comment posted"
+    else
+      redirect_to root_path, alert: "Your comment was not posted"
+    end
+  end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
