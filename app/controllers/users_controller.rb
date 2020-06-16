aec4747f3d31efa03975467c8d015e4608666c1f
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-16 01:38:17 +0100

    add user image edit routes and also style it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def new_avatar
+    @user = current_user
+  end
+
+  def update_avatar
+    @user = current_user
+    if @user.update(avatar: params[:user][:avatar])
+      redirect_to root_path, notice: 'avatar was updated successfully.'
+    else
+      redirect_to root_path, alert: 'avatar wasn\'t updated for some raison.'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
+  get '/new_avatar', to: 'users#new_avatar', as: :new_avatar
+  patch '/update_avatar', to: 'users#update_avatar', as: :update_avatar
+
   devise_for :users
   resources :posts
 end
