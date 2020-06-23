cdc5a4c1e40a6bbcddd61bbc9b2df3f5da7e0b1f
FrancoRosa <km115.franco@gmail.com>
2020-06-23 12:49:40 -0500

    Add Reviews controller

--- db/migrate/20200623174530_create_reviews.rb
+++ db/migrate/20200623174530_create_reviews.rb
@@ -2,8 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.string :text
-      t.references :user, null: false, foreign_key: true
       t.integer :rate
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
