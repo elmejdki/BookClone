de2917fcfc4b5e74b4b39cc23c95a2b379b1489d
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-24 02:56:52 +0100

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar,:cover_picture, :fullname, :bio, :city, :gender, :profession])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar, :cover_picture, :fullname, :bio, :city, :gender, :profession])
   end
 
   def check_if_there_is_new_messages
