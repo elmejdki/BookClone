ee82805db00dd9186f7ed8ee633f75318403941f
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-24 02:31:16 +0100

    Cancel the last commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,10 +61,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  config.assets.image_optim.nice = 20
-  config.assets.image_optim.svgo = false 
-  config.assets.image_optim.gifsicle.careful = true
-  config.assets.image_optim.verbose = true
-
 end
