7fce882ef6a52446c1e9844d710afacdfffcff76
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-24 02:08:53 +0100

    Prevent empty messages to be sent

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -17,26 +17,28 @@ document.addEventListener('turbolinks:load', () => {
     },
 
     received(data) {
-      const user_id = Number(document.getElementById('user-id').getAttribute('data-user-id'));
-
-      const messages_container = document.getElementById('messages');
-      const message = `
-      <div class="wrapper ${data.message.user_id == user_id ? 'me' : ''}">
-        <div class="message">
-          ${data.message.body}
-        </div>
-        <div class="date">
-          1 minute
-        </div>
-      </div>`;
-
-      messages_container.innerHTML = messages_container.innerHTML + message;
-
-      const message_input = document.getElementById('message_body');
-      message_input.value = '';
-
-      document.querySelector(".messages-body").scrollTo(0,
-        document.querySelector(".messages-body").scrollHeight);
+      if(data.message.body != '') {
+        const user_id = Number(document.getElementById('user-id').getAttribute('data-user-id'));
+  
+        const messages_container = document.getElementById('messages');
+        const message = `
+        <div class="wrapper ${data.message.user_id == user_id ? 'me' : ''}">
+          <div class="message">
+            ${data.message.body}
+          </div>
+          <div class="date">
+            1 minute
+          </div>
+        </div>`;
+  
+        messages_container.innerHTML = messages_container.innerHTML + message;
+  
+        const message_input = document.getElementById('message_body');
+        message_input.value = '';
+  
+        document.querySelector(".messages-body").scrollTo(0,
+          document.querySelector(".messages-body").scrollHeight);
+      }
     }
   });
 });
