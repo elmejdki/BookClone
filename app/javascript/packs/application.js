327430beafb1a22c959c60014abb1e5ca5947b68
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-25 20:32:55 +0100

    Add a modal to update profile piture and cover pic

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,21 @@ require("channels")
 document.addEventListener('turbolinks:load', () => {
   const post_dropdowns = document.getElementsByClassName('dropdown-trigger');
   const dropdowns = document.getElementsByClassName('dropdown');
+  const avatar_modal = document.getElementById('avatar-modal');
+  const avatar_modal_button = document.getElementById('show-avatar-modal');
+  const cover_modal = document.getElementById('cover-modal');
+  const cover_modal_button = document.getElementById('show-cover-modal');
+  const avatar_background = document.getElementById('avatar-modal-background');
+  const avatar_close = document.getElementById('avatar-modal-close');
+  const cover_background = document.getElementById('cover-modal-background');
+  const cover_close = document.getElementById('cover-modal-close');
+  const fileInput_3 = document.querySelector('#image-upload-js-3 input[type=file]');
+  const fileInput_4 = document.querySelector('#image-upload-js-4 input[type=file]');
+  const rooms_links = document.getElementsByClassName('room-linker');
+  const notificaton_btn = document.getElementById('delete');
+  const messages_body = document.querySelector(".messages-body");
+  const message_menu = document.getElementById('message_menu');
+  const fileInput = document.querySelector('#image-upload-js input[type=file]');
 
   const toggleMenu = (e) => {
     e.target.parentElement.parentElement.parentElement.classList.toggle('is-active');
@@ -42,7 +57,6 @@ document.addEventListener('turbolinks:load', () => {
     return false;
   })
 
-  const rooms_links = document.getElementsByClassName('room-linker')
   Array.prototype.slice.call(rooms_links).forEach(room_link => {
     room_link.addEventListener('click', reload_page);
   });
@@ -53,32 +67,63 @@ document.addEventListener('turbolinks:load', () => {
 
   document.addEventListener('click', hideMenu);
 
-  const notificaton_btn = document.getElementById('delete')
-
   if(notificaton_btn){
     notificaton_btn.addEventListener('click', (e) => {
       e.target.parentElement.style.display = 'none'
     })
   }
 
-  const messages_body = document.querySelector(".messages-body");
   if (messages_body){
     messages_body.scrollTo(0,
       document.querySelector(".messages-body").scrollHeight);
   }
 
-  const message_menu = document.getElementById('message_menu')
   message_menu.addEventListener('click', (e) => {
     e.target.classList.remove('message_notice')
   });
 
-  const fileInput = document.querySelector('#image-upload-js input[type=file]');
   if(fileInput) {
     fileInput.onchange = () => {
       if (fileInput.files.length > 0) {
         const fileName = document.querySelector('#image-upload-js .file-name');
         const icon = document.querySelector("#image-upload-js .file-icon").style.color = "#48c774";
         fileName.textContent = fileInput.files[0].name;
+      }
+    }
+  }
+
+  if(avatar_modal) {
+    const close_action = () => {
+      avatar_modal.classList.remove('is-active');
+      cover_modal.classList.remove('is-active');
+    }
+    
+    avatar_modal_button.addEventListener('click', () => {
+      avatar_modal.classList.add('is-active');
+    });
+
+    cover_modal_button.addEventListener('click', () => {
+      cover_modal.classList.add('is-active');
+    });
+
+    avatar_close.addEventListener('click', close_action);
+    avatar_background.addEventListener('click', close_action);
+    cover_close.addEventListener('click', close_action);
+    cover_background.addEventListener('click', close_action);
+    
+    fileInput_3.onchange = () => {
+      if (fileInput_3.files.length > 0) {
+        const fileName = document.querySelector('#image-upload-js-3 .file-name');
+        const icon = document.querySelector("#image-upload-js-3 .file-icon").style.color = "#48c774";
+        fileName.textContent = fileInput_3.files[0].name;
+      }
+    }
+
+    fileInput_4.onchange = () => {
+      if (fileInput_4.files.length > 0) {
+        const fileName = document.querySelector('#image-upload-js-4 .file-name');
+        const icon = document.querySelector("#image-upload-js-4 .file-icon").style.color = "#48c774";
+        fileName.textContent = fileInput_4.files[0].name;
       }
     }
   }
